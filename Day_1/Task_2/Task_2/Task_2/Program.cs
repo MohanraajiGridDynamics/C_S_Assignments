efd9a5308eef77d18ec8bf4144644278b926808e
Mohanraaji B <mmohanraaji@griddynamics.com>
2025-03-18 11:52:26 +0530

    Completed Task 2

---
+++ Day_1/Task_2/Task_2/Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length > 0)
+        {
+            string name = args[0];
+            Console.WriteLine($"Hello, {name}!");
+        }
+        else
+        {
+            Console.WriteLine("No username provided.");
+        }
+    }
+}
+//We have to declare arguments at the start of the creation of the project.
